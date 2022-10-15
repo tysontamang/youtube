c56a409166ed2ee523b3faae7107ea94fbc5262c
taisan tamang <54075970+tysontamang@users.noreply.github.com>
2022-10-15 12:03:56 +0545

    added follow ui

--- youtube.sh
+++ youtube.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 pip install pytube
+
+echo ""
+
+echo -e "\e[1;91m╔══╗\e[1;92m┌──┐┬   ┬   ┌──┐┬ ┬ ┬"
+
+echo -e "\e[1;91m║   \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m╠═╣ \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m║   \e[1;92m└──┘┴──┘┴──┘└──┘└─┴─┘"
+
+echo -e "\e[1;91m╩\e[1;92m     \e[1;92mCOMPUTER ENGINEERING"
+
+
 echo "press 1 for video, 2 for mp3 and 3 for playlist: "
 read x
 if [ $x == 1 ]; then
